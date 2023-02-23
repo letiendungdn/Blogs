69962f138baaef0311ed43b100fa0b3d3ad3198f
Dung Le <letiendungdt>
2023-02-23 14:15:45 +0700

    ok

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -23,7 +23,7 @@ router.post("/add-blog", authMiddleware, async (req, res) => {
 // get all blogs
 router.get("/get-all-blogs", async (req, res) => {
   try {
-    const blogs = await Blog.find();
+    const blogs = await Blog.find().populate("user");
     res.send({
       message: "Blogs fetched successfully",
       data: blogs,
@@ -40,10 +40,42 @@ router.get("/get-all-blogs", async (req, res) => {
 // get blog by id
 router.get("/get-blog-by-id/:id", async (req, res) => {
   try {
-    const blog = await Blog.findById(req.params.id);
+    const blog = await Blog.findById(req.params.id).populate("user");
     res.send({
       message: "Blogs fetched successfully",
       data: blog,
+      success: true,
+    });
+  } catch (error) {
+    res.send({
+      error: error.message,
+      success: false,
+    });
+  }
+});
+
+// update blog
+router.put("/update-blog", authMiddleware, async (req, res) => {
+  try {
+    await Blog.findByIdAndUpdate(req.params.id, req.body);
+    res.send({
+      message: "Blog updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.send({
+      error: error.message,
+      success: false,
+    });
+  }
+});
+
+// delete blog
+router.delete("/delete/:id", authMiddleware, async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.send({
+      message: "Blog deleted successfully",
       success: true,
     });
   } catch (error) {
